3b825762b90bc33bc9490dd94c77823b86e2a167
José Natividad Chávez <42912960+natividev@users.noreply.github.com>
2024-02-20 20:44:22 -0600

    feat: modulo auth passport jwt (#7)
    * feat: modulo auth passport jwt
    
    * feat: env example enviroment
    
    * feat: docker install prisma

--- src/domain/auth/controller/auth.controller.ts
+++ src/domain/auth/controller/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
+import { AuthDto } from '../dto/auth.dto';
+import { SignInDto } from '../dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  saludo() {
-    return this.authService.getUser();
+  @Post('signup')
+  async signup(@Body() signupDto: AuthDto) {
+    return await this.authService.signUp(signupDto);
+  }
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
