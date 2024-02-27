5fe5ee28676f65c6fc430ff41e17248cd8cdd8d6
Luis Caceres <56177103+CaceresLuis@users.noreply.github.com>
2024-02-26 21:25:22 -0600

    feat: add patient responses (#10)
    * feat: add patient responses
    
    * feat: add patient responses

--- src/domain/patient/repository/patient.repository.ts
+++ src/domain/patient/repository/patient.repository.ts
@@ -2,46 +2,88 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreatePatientDto } from '../dto/create-patient.dto';
 import { UpdatePatientDto } from '../dto/update-patient.dto';
+import { Estado } from '../../common/enum/Estados';
 
 @Injectable()
 export class PatientRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async getPacientes() {
-    return await this.prisma.paciente.findMany({ where: { estado: true } });
+    const patients = await this.prisma.paciente.findMany({
+      where: { estado: true },
+      select: {
+        id: true,
+        nombre: true,
+        edad: true,
+        correo: true,
+        telefono: true,
+      },
+    });
+    if (patients) return patients;
+    else {
+      return { error: 'no hay paciente...' };
+    }
   }
 
   async getPacientesInactivos() {
-    return await this.prisma.paciente.findMany({ where: { estado: false } });
+    const patients = await this.prisma.paciente.findMany({
+      where: { estado: false },
+      select: {
+        id: true,
+        nombre: true,
+        edad: true,
+        correo: true,
+        telefono: true,
+      },
+    });
+    if (patients) return patients;
+    else {
+      return { error: 'no hay paciente...' };
+    }
   }
 
   async getPacienteById(pacienteId: number) {
-    return await this.prisma.paciente.findFirst({
+    const paciente = await this.prisma.paciente.findFirst({
       where: { id: pacienteId, estado: true },
     });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { estado, ...rest } = paciente;
+    if (paciente) return rest;
+    else return { error: `paciente con id: ${pacienteId} no existe` };
   }
 
   async createPaciente(dto: CreatePatientDto) {
     try {
-      return await this.prisma.paciente.create({
+      await this.prisma.paciente.create({
         data: { estado: true, ...dto },
       });
+      return { estado: Estado.Creado };
     } catch (error) {
-      throw error;
+      return { estado: Estado.Fallindo, error: error };
     }
   }
 
   async editPaciente(pacienteId: number, dto: UpdatePatientDto) {
-    await this.prisma.paciente.update({
-      where: { id: pacienteId },
-      data: { ...dto },
-    });
+    try {
+      await this.prisma.paciente.update({
+        where: { id: pacienteId },
+        data: { ...dto },
+      });
+      return { estado: Estado.Editado };
+    } catch (error) {
+      return { estado: Estado.Fallindo, error: error.meta.cause };
+    }
   }
 
   async deletePaciente(pacienteId: number) {
-    await this.prisma.paciente.update({
-      where: { id: pacienteId },
-      data: { estado: false },
-    });
+    try {
+      await this.prisma.paciente.update({
+        where: { id: pacienteId },
+        data: { estado: false },
+      });
+      return { estado: Estado.Editado };
+    } catch (error) {
+      return { estado: Estado.Fallindo, error: error.meta.cause };
+    }
   }
 }
