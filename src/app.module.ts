bbdce62e3321fd1690e8b08227c5eb29f421daa6
Natividad <josenatividadcv@gmail.com>
2024-02-26 21:43:22 -0600

    feat: modulo inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './domain/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PatientModule } from './domain/patient/patient.module';
+import { AppointmentsModule } from './domain/appointments/appointments.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PatientModule } from './domain/patient/patient.module';
     }),
     AuthModule,
     PatientModule,
+    AppointmentsModule,
   ],
 })
 export class AppModule {}
