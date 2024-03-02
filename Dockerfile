a857f5000b864e91e1bf0ead5123c2df4bd0a8b2
Natividad <josenatividadcv@gmail.com>
2024-03-01 23:48:11 -0600

    chore: docker composer

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,6 @@ RUN npx prisma generate
 
 RUN npm run build
 
-EXPOSE 3000
-
-CMD ["npm", "run", "start:prod"]
+EXPOSE 3000 
+CMD ["npm", "run", "start:dev"]
+# CMD ["npm", "run", "start:prod"]
--- src/domain/appointments/repository/appointments.repository.ts
+++ src/domain/appointments/repository/appointments.repository.ts
@@ -10,15 +10,12 @@ export class AppointmentsRepository {
     const { fechaCita, idPaciente, idMedico, observaciones } =
       createAppointmentDto;
 
-    // necesito validar si el medico tiene disponibilidad en la fecha y hora seleccionada para la cita
-
     const isDisponible = await this.prisma.citas.findFirst({
       where: {
         idMedico,
         fecha_hora: new Date(fechaCita),
       },
     });
-
     if (isDisponible) {
       return {
         message: 'El médico no está disponible en la fecha y hora seleccionada',
