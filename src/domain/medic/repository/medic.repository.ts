bcbf509a0ed2d430818c6243ef54a43c7e2e4e91
Natividad <josenatividadcv@gmail.com>
2024-07-02 22:41:00 -0600

    feat: intersertor response

--- src/domain/medic/repository/medic.repository.ts
+++ src/domain/medic/repository/medic.repository.ts
@@ -75,10 +75,21 @@ export class MedicRepository {
           actividad_economica_codigo: actividadEconomica,
         },
       });
-      return { estado: Estado.Creado };
+      return { message: 'Médico creado exitosamente' };
     } catch (error) {
-      console.log(error);
-      return { estado: Estado.Fallindo, error: error.meta.cause };
+      console.log('error', error);
+      if (error.code === 'P2003') {
+        const fieldName = error.meta?.field_name || 'Unknown field';
+        return {
+          message: `Error al crear el médico: Falló la restricción de clave externa en el campo ${fieldName}`,
+          error: error.message,
+        };
+      }
+
+      return {
+        message: 'Error al crear el médico',
+        error: error,
+      };
     }
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('/api');
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
